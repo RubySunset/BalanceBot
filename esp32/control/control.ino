c4cbf108670c852157ba2dbc01a2715474185a1b
tomytzhou <tomytzhou@gmail.com>
2023-06-08 21:40:57 +0100

    pwm controlled motors

--- esp32/control/control.ino
+++ esp32/control/control.ino
@@ -1,23 +1,28 @@
-#include <AccelStepper.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-AccelStepper s1(AccelStepper::DRIVER, 27, 26); //STRR, DIRR
-AccelStepper s2(AccelStepper::DRIVER, 25, 33); //STRL, DIRL
+const int STRR = 23;
+const int DIRR = 19;
+const int STRL = 18;
+const int DIRL = 5;
+
 
 Adafruit_MPU6050 mpu;
 
-#define DIRR 1 //set to +-1 to compensate for rotating direction
-#define DIRL 1
+
 
 double theta;
 double alpha;
 
 void setup(){
   Serial.begin(115200);  
-  s1.setMaxSpeed(700);
-  s2.setMaxSpeed(700);
+  ledcSetup(1, 100, 10);
+  ledcAttachPin(STRR, 1);
+  ledcWrite(1, 127);
+  ledcSetup(2, 100, 10);
+  ledcAttachPin(STRL, 2);
+  ledcWrite(2, 127);
   mpu.begin();
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
@@ -40,8 +45,18 @@ void loop(){
 }
 
 void setSpeed(int r, int l){
-  s1.setSpeed(r*DIRR*100/PI);
-  s2.setSpeed(l*DIRL*100/PI);
-  s1.runSpeed();
-  s2.runSpeed();
+  if(r < 0){
+    digitalWrite(DIRR, HIGH); //opposite direction as the left motor
+  }
+  if(r > 0){
+    digitalWrite(DIRR, LOW);
+  }
+  if(l < 0){
+    digitalWrite(DIRL, LOW);
+  }
+  if(l > 0){
+    digitalWrite(DIRL, HIGH);
+  }
+  ledcChangeFrequency(1, r*100/PI, 10);
+  ledcChangeFrequency(2, l*100/PI, 10);
 }
