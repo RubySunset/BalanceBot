66d64ac4242a9628105b0d6e1a08d95401dbe94d
MITeo21 <meigan.teo21@imperial.ac.uk>
2023-06-19 15:38:51 +0100

    Change light sensor for new connections

--- esp32/light_sensor/light_sensor.ino
+++ esp32/light_sensor/light_sensor.ino
@@ -1,12 +1,10 @@
-#define F1 27
-#define F2 26
-#define F3 25
-#define B1 33
-#define B2 14
-#define B3 4
-#define L 15
-#define R 2
-
+///-----LIGHT SENSOR PINS | START-----///
+#define F1 32  //A4
+#define F2 35  //A5
+#define F3 34  //A3
+#define L 33  //soldered connection
+#define R 39  //soldered connection
+///-----LIGHT SENSOR PINS | END-----///
 
 void setup() {
   Serial.begin(115200);
@@ -20,16 +18,10 @@ void loop() {
   Serial.print(analogRead(F2));
   Serial.print(" F3: ");
   Serial.print(analogRead(F3));
-  Serial.print(" B1: ");
-  Serial.print(analogRead(B1));
-  Serial.print(" B2: ");
-  Serial.print(analogRead(B2));
-  Serial.print(" B3: ");
-  Serial.print(analogRead(B3));
   Serial.print(" L: ");
   Serial.print(analogRead(L));
   Serial.print(" R: ");
   Serial.println(analogRead(R));
   
-  delay(500);
+  delay(100);
 }
