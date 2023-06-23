12888728105a17c39d61d537d2ce3a1c3a8661c8
MITeo21 <meigan.teo21@imperial.ac.uk>
2023-06-23 11:11:24 +0100

    Final course correction code

--- esp32/control/ultrasonic_cc.ino
+++ esp32/control/ultrasonic_cc.ino
@@ -6,74 +6,45 @@
 #include <AccelStepper.h>
 #include <MPU6050.h>
 
-// Ultrasonic sensor pins
-#define trigPin_L 33
-#define trigPin_F 32
-#define trigPin_R 17
-#define echoPin_L 34
-#define echoPin_F 36
-#define echoPin_R 39
+#include <NewPing.h>
 
-// Motor pins
+#define SONAR_NUM 3      // Number of sensors.
+#define MAX_DISTANCE 40 // Maximum distance (in cm) to ping.
+
+NewPing sonar[SONAR_NUM] = {   // Sensor object array.
+  NewPing(33, 34, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(32, 36, MAX_DISTANCE),
+  NewPing(17, 39, MAX_DISTANCE)
+};
+
 const int STRR = 23;
 const int DIRR = 19;
 const int STRL = 18;
 const int DIRL = 5;
-
-// Ultrasonic sensor variables - duration is in us, distance is in cm.
-float dur_F, dist_F, dur_L, dist_L, dur_R, dist_R;
-
-// For course-correction.
-float prev_dist_L = 10000, prev_dist_R = 10000;
-
 AccelStepper s1(AccelStepper::DRIVER, STRR, DIRR);  //STRR, DIRR
 AccelStepper s2(AccelStepper::DRIVER, STRL, DIRL);  //STRL, DIRL
 
 MPU6050 mpu;
-
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
-
 double theta_dash;
 double theta = 0;
 double alpha_dash;
 double alpha = 0;
 double integral_az = 0;
-double sensed_speed = 0;
-double positionX = 0;
-double positionY = 0;
+float sensed_speed = 0;
+float positionX = 0;
+float positionY = 0;
+float displacement = 0;
 
-//Note that for multiple PID controllers, we use an array for sum_e and e_n_1
-//index 0 is for the theta_dash controller, index 1 is for x', and index 2 is for alpha_dash
-double sum_e[3] = { 0, 0, 0 }, e_n_1[3] = { 0, 0, 0 };  //sum of errors and previous error initialisation
-double control_max = 10;                                //absolute maximum value of actuators
-double speed_max = 5;
-char movement_mode = 's';  //current movement mode of robot; s = stationary, m = moving, t = turning
-bool finished_turning = true;
+double speed_right, speed_left;
 
-//PID Output calculation
-double simple_PID_calc(double Ts, double set_point, double sensor_point, int PID_ID, double Kp, double Ki, double Kd) {
-  //variable finding
-  double e = set_point - sensor_point;        //current error for proportional term [1]
-  sum_e[PID_ID] += e;                         //cumulative error for sum term [2]
-  double differential_e = e - e_n_1[PID_ID];  //delta error for differential term [3]
-
-  //output setting
-  double control_input = Kp * e + Ki * Ts * sum_e[PID_ID] + Kd * (1 / Ts) * differential_e;                 //control law
-  if (abs(control_input) >= control_max) control_input = control_max * abs(control_input) / control_input;  //in case motor limit reached
-
-  //incrementing
-  e_n_1[PID_ID] = e;
-
-  //return
-  return control_input;
-}
+unsigned long prevTime = 0;
 
 bool sample_flag = false;
 bool sample_flag_2 = false;
 hw_timer_t* sample_time = NULL;
 hw_timer_t* sample_time_2 = NULL;
-double sample_interval = 0.002;
 
 void IRAM_ATTR doSample() {
   sample_flag = true;
@@ -83,15 +54,11 @@ void IRAM_ATTR doSample2() {
   sample_flag_2 = true;
 }
 
-double control_speed = 0;
-double speed_left = 0;
-double speed_right = 0;
-int cycle_number = 0;  //temp variable
-
-//These parameters (apart from theta_setpoint) will be controlled by commands received from higher levels
 double theta_setpoint = 0;
 double velocity_setpoint = 0;
-double angle_setpoint = 0;  //decide that clockwise is positive direction; so could be +20deg, -35deg, etc
+double angle_setpoint = 0;
+
+ char movement_mode = 'm'; 
 
 void setSpeed(int r, int l) {
   s1.setSpeed(r * 100 / PI);  //r*100/PI is steps per second
@@ -100,82 +67,53 @@ void setSpeed(int r, int l) {
   s2.runSpeed();
 }
 
-double distance_per_step = 0;
-//called in setup; units are metres
-double find_distance_per_step() {
-  double wheel_radius = 0.033;  //wheel radius is 33mm
-  double wheel_circumference = TWO_PI * wheel_radius;
-  // This is unnecessary to name but I'm just doing it for clarity
-  double steps_per_revolution = 200;
-  // As one revolution is done, it will go distance of 1 circumference, divided by steps per revolution would give distance per step
-  return wheel_circumference / steps_per_revolution;
-}
+// Ultrasonic sensor variables - duration is in us, distance is in cm.
+float dur_F, dist_F, dur_L, dist_L, dur_R, dist_R;
 
-double distance_moved(double wheel_speed) {
-  // if speed is in m/s, conversion to steps:
-  //double step_speed = wheel_speed / distance_per_step
-
-  //converting wheel_speed to steps per second
-  wheel_speed = wheel_speed * 800 / PI;
-
-  // Unsure on last step, speed if in steps per second * distance per step will get distance travelled in 1 second??
-  // Might work better if wheel_speed is actually steps for steps * distance per steps but I think this works
-  //double distance = wheel_speed * distance_per_step;
-  // if needed:    steps/second *   metres/step     *    seconds         = metres
-  double distance = wheel_speed * distance_per_step * sample_interval;
-  return distance;
-}
-
-//Complementary filter for alpha and theta
-const double COMPL_ALPHA = 0.98;
-const int filter_adjustment_interval = 500;
-double compl_alpha = 0;
-double calibrateMPU(){
-  for(int i = 0; i < 1000; i++){
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    compl_alpha = 0.999*(compl_alpha+gy*0.002/131)+0.001*atan2(ax,az)*RAD_TO_DEG;
-    delay(2);
+// For course-correction.
+float prev_dist_L = 10000, prev_dist_R = 10000;
+bool turning_done = false;
+int ultrasound_sequence = 0;
+void ultrasoundSample(){
+  if(ultrasound_sequence == 0){
+    dist_L = sonar[0].ping_cm();
+    ultrasound_sequence = 1;
   }
-  return compl_alpha;
-}
-double compl_theta = 0;
-
-//carries out appropriate actions once turning done
-//figure out what- send message to node? retrieve instructions from some stack?
-void check_turning_finished(double alpha_error) {
-  if (alpha_error < 0.7) {
-    Serial.println("Finished turning");
-    angle_setpoint = alpha;
-    finished_turning = true;
-    movement_mode = 'm';
-    velocity_setpoint = 0;
+  if(ultrasound_sequence == 1){
+    dist_F = sonar[1].ping_cm();
+    ultrasound_sequence = 2;
+  }
+  if(ultrasound_sequence == 2){
+    dist_R = sonar[2].ping_cm();
+    ultrasound_sequence = 0;
   }
 }
 
-void setup() {
-  // begin serial port
-  Serial.begin (9600);
+///-----COMMUNICATIONS GLOBALS & FUNCTIONS | START-----///
+const char* WIFI_SSID = "BalanceBot";
+const char* WIFI_PASS = "Ajanthan";
 
-  // configure the trigger pin to output mode
-  pinMode(trigPin_F, OUTPUT);
-  pinMode(trigPin_L, OUTPUT);
-  pinMode(trigPin_R, OUTPUT);
-  // configure the echo pins to input mode
-  pinMode(echoPin_F, INPUT);
-  pinMode(echoPin_L, INPUT);
-  pinMode(echoPin_R, INPUT);
+const int BUFFER_SIZE = 28;
+char buf[BUFFER_SIZE + 1];
+char hexBuf[BUFFER_SIZE * 2 + 1];
+char LDR_chars[2 + 1];
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+
+
+void setup(){
+  Serial.begin(115200);
   Wire.begin();
   mpu.initialize();
-  Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  //Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
   mpu.setXAccelOffset(-599);
   mpu.setYAccelOffset(-1235);
   mpu.setZAccelOffset(1196);
   mpu.setXGyroOffset(94);
   mpu.setYGyroOffset(-46);
   mpu.setZGyroOffset(22);
-
-  //alpha = calibrateMPU();
 
   pinMode(DIRR, OUTPUT);
   pinMode(DIRL, OUTPUT);
@@ -189,120 +127,133 @@ void setup() {
 
   sample_time_2 = timerBegin(1, 80, true);
   timerAttachInterrupt(sample_time_2, &doSample2, true);
-  timerAlarmWrite(sample_time_2, 20000, true);  //sampling time in microseconds
+  timerAlarmWrite(sample_time_2, 30000, true);  //sampling time in microseconds
   timerAlarmEnable(sample_time_2);
 
-  distance_per_step = find_distance_per_step();
+  xTaskCreatePinnedToCore(ControlLoop, "Task1", 8000, NULL, 2, &Task1, 1);
+  xTaskCreatePinnedToCore(CommunicationsLoop, "Task2", 8000, NULL, 1, &Task2, 0);
+ 
 }
 
-void loop() {
-  digitalWrite(trigPin_F, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin_F, LOW);
-  dur_F = pulseIn(echoPin_F, HIGH);
-  dist_F = 0.017 * dur_F;
+double control_speed = 2;
+void loop() {}
 
-  digitalWrite(trigPin_L, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin_L, LOW);
-  dur_L = pulseIn(echoPin_L, HIGH);
-  dist_L = 0.017 * dur_L;
+void ControlLoop(void* pvParameters) {
+  while (true) {
+    velocity_setpoint = -3;
+//    angle_setpoint = 0;
+    if(sample_flag == true){
+      unsigned long currentTime = millis();
+      float elapsed_time = currentTime - prevTime;
+      prevTime = currentTime;
 
-  digitalWrite(trigPin_R, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin_R, LOW);
-  dur_R = pulseIn(echoPin_R, HIGH);
-  dist_R = 0.017 * dur_R;
-
-  // print the value to Serial Monitor
-//   Serial.print("Front distance: ");
-//   Serial.print(dist_F);
-//   Serial.println(" cm");
-//   Serial.print("Left distance: ");
-//   Serial.print(dist_L);
-//   Serial.println(" cm");
-//   Serial.print("Right distance: ");
-//   Serial.print(dist_R);
-//   Serial.println(" cm");
-
-  if (sample_flag == true) {
       //Sensor data
       mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
       //angle speeds in degrees/s
       theta_dash = -gx / 131;
       alpha_dash = -gz / 131;
-      theta += sample_interval * theta_dash;
-      alpha += sample_interval * alpha_dash;
+      float elapsed_time_seconds = elapsed_time / 1000;
+      theta += elapsed_time_seconds * theta_dash;
+      alpha += elapsed_time_seconds * alpha_dash;
 
-      //alpha complementary filter
-      int accy = ay/1638;
-      int accz = az/1638;   
-      compl_alpha = COMPL_ALPHA*(compl_alpha + alpha_dash*0.002)-(1-COMPL_ALPHA)*(atan2(accy,accz)+PI)*RAD_TO_DEG;
-      if(k>filter_adjustment_interval){
-        alpha = compl_alpha;
-        k=0;
-      }
-      compl_theta = 0.98*(compl_theta+gx*0.002/131)+0.02*atan(ay / sqrt(pow(ax, 2.0) + pow(az, 2.0)))*RAD_TO_DEG;
 
-      //velocity and position in m/s and m. displacement is since last sample
-      double displacement = distance_moved(control_speed);
-      sensed_speed = displacement / sample_interval;
+      
+      sensed_speed = control_speed * 8 * 0.033;
+      displacement =  sensed_speed * elapsed_time_seconds;
       positionX += displacement * sin(alpha * PI / 180);
       positionY += displacement * cos(alpha * PI / 180);
 
-    //   // Basic course correction
-    //   if (movement_mode == 'm') {
-    //     if (dist_L <= 10) {
-    //         movement_mode = 't';
-    //         angle_setpoint = alpha + 10;
-    //     } else if (dist_R <= 10) {
-    //         movement_mode = 't';
-    //         angle_setpoint = alpha - 10;
-    //     }
-    //   }
-    }
-
-    if (movement_mode == 'm') {
-        if (dist_L <= 10) {
-            if (dist_L - prev_dist_L < 0) {
-                movement_mode = 't';
-                angle_setpoint = alpha + 5;
-                prev_dist_L = dist_L;
-            } else {
-                prev_dist_L = -10000;
-                prev_dist_R = 10000;
-            }
-        } else if (dist_R <= 10) {
-            if (dist_R - prev_dist_R < 0) {
-                movement_mode = 't';
-                angle_setpoint = alpha - 5;
-                prev_dist_R = dist_R;
-            } else {
-                prev_dist_R = -10000;
-                prev_dist_L = 10000;
-            }
+      if (movement_mode == 'm') {
+        speed_right = velocity_setpoint;
+        speed_left = velocity_setpoint;
+        } 
+      else if (movement_mode == 't') {
+        double angle_diff = angle_setpoint - alpha;
+        if(angle_diff > 1){
+          speed_right = -0.5;
+          speed_left = 0.5;
         }
-    }
-
-    // Assume stabilising wheels and frame attached
-    if (movement_mode == 'm') {
-        //control_speed = simple_PID_calc(sample_interval, velocity_setpoint, sensed_speed, 1, 1, 0.5, 0.01); //values to be tuned
-        control_speed = velocity_setpoint;
-        speed_right = control_speed;
-        speed_left = control_speed;
-    } else if (movement_mode == 't') {
-        finished_turning = false;
-        double wheel_speed_difference = simple_PID_calc(sample_interval, angle_setpoint, alpha, 2, 0.01, 0, 0);
-        check_turning_finished(abs(alpha - angle_setpoint));
-        speed_right -= wheel_speed_difference;
-        speed_left += wheel_speed_difference;
-        control_speed = 0;
-    } else {
+        else if(angle_diff < -1){
+          speed_right = 0.5;
+          speed_left = -0.5;
+        }
+        else{
+           movement_mode = 'm';
+//          turning_done = true;
+        }
+        
+        
+      }
+      else {
         speed_right = 0;
         speed_left = 0;
+      }
+
+      
+      sample_flag = false;
     }
-    sample_flag = false;
-    //Set speed and increment loop number
+
     setSpeed(speed_right * 8, speed_left * 8);
-    //cycle_number += 1;
+  }
+}
+
+///-----COMMUNICATIONS LOOP | START-----///
+
+
+const unsigned long SEND_INTERVAL_MS = 200;
+static unsigned long lastSendMillis = 1;
+void CommunicationsLoop(void* pvParameters) {
+  int ultrasound_sequence = 0;
+  while (true) {
+    if (sample_flag_2 == true) {
+//       delay(20);
+
+    if(ultrasound_sequence == 0){
+      dist_L = sonar[0].ping_cm();
+      Serial.println(dist_L);
+      ultrasound_sequence = 1;
+    }
+    if(ultrasound_sequence == 1){
+      dist_F = sonar[1].ping_cm();
+      Serial.println(dist_F);
+      ultrasound_sequence = 2;
+    }
+    if(ultrasound_sequence == 2){
+      dist_R = sonar[2].ping_cm();
+      Serial.println(dist_R);
+      ultrasound_sequence = 0;
+    }
+
+      if (dist_F > 10 || dist_F == 0) {
+        if (dist_L < 10 && dist_L != 0) {
+            movement_mode = 't';
+            angle_setpoint = alpha + 50/dist_L;
+//            Serial.println("Turning right");
+            turning_done = false;
+        } 
+        else if (dist_R < 10 && dist_R != 0) {
+            movement_mode = 't';
+            angle_setpoint = alpha - 50/dist_R;
+//            Serial.println("Turning left");
+            turning_done = false;
+        }
+      }
+      else{
+        if(dist_L > 30 || dist_L == 0){
+          movement_mode = 't';
+          angle_setpoint = alpha - 90;
+          turning_done = false;
+        }
+        if(dist_R > 30 || dist_R == 0){
+          movement_mode = 't';
+          angle_setpoint = alpha + 90;
+          turning_done = false;
+        }
+      } 
+        
+      
+
+      sample_flag_2 = false;
+    }
+  }
 }
